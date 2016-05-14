0a6b5a4d3c4de9e49ceb69e520282ebdeb5758f1
Sachin Sharma <sachins1211@gmail.com>
2016-05-14 18:26:49 +0530

    Added Interstitial Ad

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,20 +7,51 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.sachin.androidjokes.JokeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     ProgressBar mProgressBar;
+    boolean mIsAppPaidVersion;
+    InterstitialAd mInterstitialAd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        mIsAppPaidVersion = getResources().getBoolean(R.bool.is_paid_version);
+
+        if (!mIsAppPaidVersion) {
+            mInterstitialAd = new InterstitialAd(this);
+            mInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+            mInterstitialAd.setAdListener(new AdListener() {
+                @Override
+                public void onAdClosed() {
+                    requestNewInterstitial();
+                    getAndDisplayJoke();
+                }
+            });
+            requestNewInterstitial();
+        }
+    }
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mInterstitialAd.loadAd(adRequest);
+
     }
 
     public void tellJoke(View view){
-        getAndDisplayJoke();
+        if (!mIsAppPaidVersion && mInterstitialAd.isLoaded())
+            mInterstitialAd.show();
+        else {
+            getAndDisplayJoke();
+        }
     }
 
     private void getAndDisplayJoke() {
@@ -34,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                     intent.putExtra(JokeActivity.EXTRA_JOKE, result);
                     startActivity(intent);
                 } else {
-                    Toast.makeText(MainActivity.this, "Error fetching Jokes from server!", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, getString(R.string.fetch_error), Toast.LENGTH_LONG).show();
                 }
                 mProgressBar.setVisibility(View.GONE);
             }
